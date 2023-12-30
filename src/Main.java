19fa6350854444f55f07d3005b6c711473274f7b
antonioap101 <121720588+antonioap101@users.noreply.github.com>
2023-12-30 22:47:50 +0000

    Finished Kata 1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+import software.ulpgc.kata1.Person;
+
+import java.time.LocalDate;
+
 public class Main {
-    public static void main(String[] args) {System.out.println("Hello world!"); }
+    public static void main(String[] args) {
+        Person daniel = new Person(LocalDate.of(2000, 2, 15), "Daniel");
+
+        System.out.println(daniel);
+    }
 }
